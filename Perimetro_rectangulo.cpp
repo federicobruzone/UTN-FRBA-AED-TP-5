60a1effd303c8ccc6fa9c4e2d27cad346d0d3ff9
federicobruzone <federicobruzone@users.noreply.github.com>
2015-05-09 14:24:29 -0300

    Update Perimetro_rectangulo.cpp

--- Perimetro_rectangulo.cpp
+++ Perimetro_rectangulo.cpp
@@ -1,4 +1,3 @@
-```c++
 #include <iostream>
 
 using namespace::std;
@@ -27,4 +26,3 @@ cout << "Altura del rectangulo. ";
 double altura = GetNumeroPositivo();
 cout << "El perimetro del rectangulo es: " << GetPerimetroRectangulo(base, altura);
 }
-```

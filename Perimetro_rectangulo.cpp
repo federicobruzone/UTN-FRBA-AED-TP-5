65ebc22006fe69ae1b2f46f1c79845a030230203
federicobruzone <federicobruzone@users.noreply.github.com>
2015-05-09 14:22:53 -0300

    Create Perimetro_rectangulo.cpp

---
+++ Perimetro_rectangulo.cpp
@@ -0,0 +1,30 @@
+'''c++
+#include <iostream>
+
+using namespace::std;
+double GetNumeroPositivo();
+double GetPerimetroRectangulo(double base, double altura);
+
+double GetNumeroPositivo(){
+	double x;
+	
+	do {
+	cout << "Ingrese un numero real positivo: ";
+	cin >> x;
+		if (x > 0)
+			return x;
+	} while (1 != 0);
+}
+
+double GetPerimetroRectangulo (double base, double altura){
+	return (2 * (base + altura));
+}
+
+int main() {
+cout << "Base del rectangulo. ";
+double base = GetNumeroPositivo();
+cout << "Altura del rectangulo. ";
+double altura = GetNumeroPositivo();
+cout << "El perimetro del rectangulo es: " << GetPerimetroRectangulo(base, altura);
+}
+'''
